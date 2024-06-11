4bc92ab187520328144994e59c77fa0dca383d49
Toney Mathews <toneymathews@gmail.com>
2024-06-10 22:01:26 -0400

    Add linked list

--- linked_list.rb
+++ linked_list.rb
@@ -7,48 +7,76 @@ gemfile do
 end
 
 class Node
-  attr_accessor :nxt, :value
-  def initialize(nxt, value)
-    @nxt = nxt
+  attr_accessor :tail
+  attr_reader :value
+  def initialize(value = nil)
     @value = value
+    @tail = nil
+  end
+
+  def to_s
+    "Node with value: #{@value}"
   end
 end
 
-class LinkedList
-  attr_accessor :node
-  def initialize(node)
-    @node = node
+class List
+  def initialize
+    @head = nil
+  end
+
+  def append(value)
+    unless @head
+      @head = Node.new(value)
+    else
+      find_tail.tail = Node.new(value)
+  end
+
+  def append_after(target, value)
+    node = find(target)
+
+    return unless node
+    
+    old_node = node.tail
+    node.tail = Node.new(value)
+    node.tail.tail = old_node
+  end
+
+  def find(value)
+    node = @head
+    return false unless @head.tail
+    return node if node.value == value
+
+    while (node = node.tail)
+      return node if node.value == value
+    end
+  end
+
+  def find_tail
+    node = @head
+    return node unless node.tail
+    while (node = node.tail)
+      return node unless node.tail
+    end
+  end
+
+  def delete(value)
+    if @head.value == value
+      @head = @head.next if @head.next
+      return
+    end
+
+    node = find_before(value)
+    node.tail = node.tail.tail
+  end
+
+  def find_before(value)
+    node = @head
+    return nil if !node.tail
+    return node if node.tail.value == value
+
+    while node = node.tail
+      return node if node.tail && node.tail.value == value
+    end
   end
 end
 
-n = Node.new(nil, 24)
-node = Node.new(n, 23)
-list = LinkedList.new(node)
-n.nxt = Node.new(nil, 25)
-
-def print_all(node)
-  p node.value
-  print_all(node.nxt) if node.nxt
-end
-
-print_all(list.node)
-
-# The below code gave an endless loop, not sure why. something about the while loop is off.
-# @list = Node.new(nil, nil)
-# @list.nxt = Node.new(nil, 23)
-
-# def print_node(node)
-#   p node.value
-#   nxt = node.nxt
-#   while nxt != nil
-#     print_node(nxt)
-#   end
-# end
-
-# count = 1
-# p "i;m here"
-
-# if count == 1 
-#   print_node(@list)
-#   count += 1
-# end
